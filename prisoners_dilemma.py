97d3aeb2a27310245d3b2a38dfbe7d0a3b1bae95
Classified <TransonP@gmail.com>
2015-02-24 15:48:13 -0800

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -599,14 +599,20 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 	#
 	elif player == 19:
 		if getting_team_name:
-			return 'loyal vengeful'
+			return 'Bianca Bondoc, Transon Pho'
 		else:
-			if len(opponent_history)==0: #It's the first round: collude
-				return 'c'
-			elif history[-1]=='c' and opponent_history[-1]=='b':
-				return 'b' # betray is they were severely punished last time
-			else:
-				return 'c' #otherwise collude
+			 if len(opponent_history)==0: #It's the first round: collude
+                		return 'c'
+            		elif history[-1]=='c' and opponent_history[-1]=='b':
+                		return 'b'
+            		elif history[-1]=='c' and opponent_history[-1]=='c':
+                		return'c'
+            		elif history[-1]=='b' and opponent_history[-1]=='b':
+                		return 'b'
+            		elif history[-1]=='b' and opponent_history[-1]=='c':
+                		return 'c'
+            		else:
+                		return 'b'
 				
 				
 	elif player == 20:
