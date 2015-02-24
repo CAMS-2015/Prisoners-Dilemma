95c54cf6b296128ef99f6305f80d609faa3a6824
StephanieBecerra <StephanieBecerra@users.noreply.github.com>
2015-02-24 15:45:26 -0800

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -533,7 +533,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 	#
 	elif player == 16:
 		if getting_team_name:
-			return 'loyal vengeful'
+			return 'Stephanie Becerra'
 		else:
 			if len(opponent_history)==0: #It's the first round: collude
 				return 'c'
